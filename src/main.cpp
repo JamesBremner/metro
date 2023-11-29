d648a484f2182d98833ca025a7318c204797e329
JamesBremner <james@ravenspoint.com>
2023-11-29 14:30:36 -0500

    Dijkstra

--- src/main.cpp
+++ src/main.cpp
@@ -4,31 +4,131 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <wex.h>
-#include "cStarterGUI.h"
 
-class cGUI : public cStarterGUI
+#include "GraphTheory.h"    https://github.com/JamesBremner/PathFinder
+
+raven::graph::sGraphData gd;
+
+std::vector<std::string> theStations;
+std::vector < std::pair<int, int>> theLines;
+
+void displayLines()
 {
-public:
-    cGUI()
-        : cStarterGUI(
-              "Starter",
-              {50, 50, 1000, 500}),
-          lb(wex::maker::make < wex::label >(fm))
+    for( auto& p : theLines )
+        std::cout << p.first <<" "<< p.second << ", ";
+    std::cout << "\n";
+}
+
+void parseName( const std::string& line )
+{
+    int vi = atoi( line.c_str());
+    std::string name = line.substr(5);
+    theStations.push_back( name );
+}
+
+void parselines(const std::vector<std::string> &vtoken)
+{
+    int s = atoi(vtoken[0].c_str());
+    int t = atoi(vtoken[1].c_str());
+    if (s > t)
+        return;
+    theLines.push_back(std::make_pair(s, t));
+}
+
+void parselink(const std::vector<std::string> &vtoken)
+{
+    int ei = gd.g.add( vtoken[0],vtoken[1] );
+    if( ei >= gd.edgeWeight.size() )
+        gd.edgeWeight.resize(1 + 2*ei );
+    int w = atoi( vtoken[2].c_str());
+    if( w == -1 )
+        w = 90;
+    gd.edgeWeight[ei] = w;
+}
+
+void displayEdges()
+{
+    for( auto& e : gd.g.edgeList() )
+        std::cout << "l "<< e.first <<" "<< e.second 
+        << " " << gd.edgeWeight[gd.g.find( e.first,e.second)] << "\n";
+}
+
+void readfile()
+{
+    enum class eInput
     {
-        lb.move(50, 50, 100, 30);
-        lb.text("Hello World");
+        head,
+        names,
+        links,
+        lines
+    };
+    eInput state = eInput::head;
 
-        show();
-        run();
+    std::ifstream ifs("../dat/metro.txt");
+    if (!ifs.is_open())
+        throw std::runtime_error("Cannot open input file");
+    std::string line;
+
+    while (getline(ifs, line))
+    {
+        std::vector<std::string> vtoken;
+        std::stringstream sst(line);
+        std::string a;
+        while (getline(sst, a, ' '))
+            if (a.length())
+                vtoken.push_back(a);
+
+
+        switch (state)
+        {
+
+        case eInput::head:
+            state = eInput::names;
+            break;
+
+        case eInput::names:
+            if (line[0] == '$') {
+                state = eInput::links;
+                break;
+            }
+            parseName(line);
+            break;
+
+        case eInput::links:
+            // if (vtoken[2] == "-1")
+            // {
+            //     state = eInput::lines;
+            //     parselines(vtoken);
+            //     break;
+            // }
+            parselink( vtoken);
+            break;
+
+        case eInput::lines:
+            parselines(vtoken);
+            break;
+        }
     }
+}
+/// @brief Find shotest path between stations
+/// @param start index of start station
+/// @param end index of end station
+/// Use Dijsktra algorithm implemented by the PathFinder library https://github.com/JamesBremner/PathFinder
 
-private:
-    wex::label &lb;
-};
+void ShortestPath( int start, int end )
+{
+    gd.startName = std::to_string( start );
+    gd.endName = std::to_string( end );
+    auto p = path( gd );
+    for( int vi : p.first )
+        std::cout << theStations[vi] << " -> ";
+}
 
 main()
 {
-    cGUI theGUI;
+    readfile();
+    // displayLines();
+    // displayEdges();
+    ShortestPath(1,12);
     return 0;
 }
